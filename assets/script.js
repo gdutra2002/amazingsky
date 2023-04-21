4312b8659ddc4b8178dcb1c5d0d80c1b79e2d98c
gdutra2002 <119914021+gdutra2002@users.noreply.github.com>
2023-04-20 23:08:20 -0700

    deploying

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,54 @@ var commentInputEl = $('#comment-input');
 
 var locationDisplayEl = $('#locationDisplay');
 
+//weather data
+
+function renderCurrentWeather(city, weather) {
+  var date = dayjs().format('M/D/YYYY');
+  // Store response data from our fetch request in variables
+  var tempF = weather.main.temp;
+  var windMph = weather.wind.speed;
+  var humidity = weather.main.humidity;
+  var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+  var iconDescription = weather.weather[0].description || weather[0].main;
+
+  var card = document.createElement('div');
+  var cardBody = document.createElement('div');
+  var heading = document.createElement('h2');
+  var weatherIcon = document.createElement('img');
+  var tempEl = document.createElement('p');
+  var windEl = document.createElement('p');
+  var humidityEl = document.createElement('p');
+
+  card.setAttribute('class', 'card');
+  cardBody.setAttribute('class', 'card-body');
+  card.append(cardBody);
+
+  heading.setAttribute('class', 'h3 card-title');
+  tempEl.setAttribute('class', 'card-text');
+  windEl.setAttribute('class', 'card-text');
+  humidityEl.setAttribute('class', 'card-text');
+
+  heading.textContent = `${city} (${date})`;
+  weatherIcon.setAttribute('src', iconUrl);
+  weatherIcon.setAttribute('alt', iconDescription);
+  weatherIcon.setAttribute('class', 'weather-img');
+  heading.append(weatherIcon);
+  tempEl.textContent = `Temp: ${tempF}°F`;
+  windEl.textContent = `Wind: ${windMph} MPH`;
+  humidityEl.textContent = `Humidity: ${humidity} %`;
+  cardBody.append(heading, tempEl, windEl, humidityEl);
+
+  todayContainer.innerHTML = '';
+  todayContainer.append(card);
+}
+
+
+
+
+
+// hx below
+
 var printQueryData = function (name, comment) {
   var cardColumnEl = $('<div>');
   cardColumnEl.addClass('col-12 col-sm-4 col-md-3');
@@ -58,6 +106,11 @@ var handleFormSubmit = function (event) {
 
 formEl.on('submit', handleFormSubmit);
 
+function renderItems(city, data) {
+  renderCurrentWeather(city, data.list[0], data.city.timezone);
+  renderForecast(data.list);
+}
+
 function fetchWeather(location) {
   console.log("I'm inside function fetchWeather");
   var { lat } = location;
@@ -72,14 +125,14 @@ function fetchWeather(location) {
     })
     .then(function (data) {
       console.log(data);
-      // renderItems(city, data);
+       renderItems(city, data);
     })
     .catch(function (err) {
       console.error(err);
     });
 }
 
-search => name
+// search => name
 
 function fetchCoords(name) {
   var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${name},USA&limit=5&appid=3fade8c75faf1243c424955cb9eafdfa`;
